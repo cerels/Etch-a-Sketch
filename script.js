a7a9cbc256c1842277f1e1ca2ebcd30eb4229970
cerels <c.albertocaro@gmail.com>
2024-04-10 19:36:23 -0500

    grid color is random on hover

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const squareGrid = document.getElementById('squareGrid');
-const squareElement = document.querySelector('.square');
-makeGrid(16)
 
+const btn = document.getElementById('btn');
+
+
+makeGrid(16);
+
+btn.addEventListener('click', () => {
+    gridLayout = window.prompt("how big should the grid be?", 16);
+    cleanGrid();
+    if (gridLayout < 100)
+        makeGrid(gridLayout);
+    else
+        makeGrid(100);
+
+});
 
 
 function makeGrid(gridSize) {
@@ -12,6 +24,23 @@ function makeGrid(gridSize) {
         square.classList.add("square");
         square.style.width = squareWidth;
         squareGrid.appendChild(square);
+
+        // add an event listener to change color when hovering
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = getRandomHexColor();
+        });
     }
 }
 
+function cleanGrid() {
+    squareGrid.innerHTML = '';
+};
+
+function getRandomHexColor() {
+    // Generate a random integer between 0 and 16777215 (decimal representation of ffffff)
+    const randomInt = Math.floor(Math.random() * 16777216);
+    // Convert the random integer to hexadecimal and pad with zeros to ensure it's six digits
+    const hexColor = randomInt.toString(16).padStart(6, '0');
+    // Return the hexadecimal color code
+    return `#${hexColor}`;
+}
